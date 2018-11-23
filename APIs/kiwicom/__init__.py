fc3fd43ef2506e31ed4db9464c39c46d4ee0dde8
martin-baro <martin.baro.cc@gmail.com>
2018-11-23 13:49:51 +0000

    Added kiwicom api python wrapper from github with it's requirements

---
+++ APIs/kiwicom/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = "Andrii But"
+__email__ = "blayson4@gmail.com"
+__from__ = "https://github.com/kiwicom/kiwicom-python"
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from APIs import kiwicom
+from APIs.kiwicom import kiwi
 
 class App():
     def __init__(self):
         pass
 
     def run(self, dest, start_d, return_d):
-        kiwicom.flight_search(dest,start_d, return_d)
+        s = kiwi.Search()
+        print("search done")
+        return
 
 
 if __name__ == "__main__":
@@ -14,3 +16,4 @@ if __name__ == "__main__":
     start_date = "04/04/2019"
     return_date = "24/04/2019"
     app.run(destination, start_date, return_date)
+    print("all done")
